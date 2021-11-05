ded0f930bd72217bd9bf79a0b5044f15c17b00b2
yarmail <yarmail@yandex.ru>
2021-11-05 10:04:36 +0300

    2. Обеспечить остановку потребителя. [#66825 #224248]

--- src/main/java/blockingqueue/ParallelSearchB.java
+++ src/main/java/blockingqueue/ParallelSearchB.java
@@ -45,12 +45,6 @@ public class ParallelSearchB {
                             e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                            Thread.currentThread().interrupt();
-                        }
                     }
                     consumer.interrupt();
                 }
